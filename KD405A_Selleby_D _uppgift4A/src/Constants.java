d9c85f737d4d517280d002887bb5a5e80e9d9b9a
pUtatis <daniel.selleby@gmail.com>
2016-02-12 11:52:17 +0100

    Update Constants.java

--- KD405A_Selleby_D _uppgift4A/src/Constants.java
+++ KD405A_Selleby_D _uppgift4A/src/Constants.java
@@ -1,5 +1,9 @@
+// NOT USED!
+
 public class Constants {
 	  
 	 /**Limits the minimum amount of characters for Human name to 3*/
-	 public static final int MIN_CHAR = 3;
+	 public static final int MIN_CHAR = 3;  
 }
+
+// NOT USED!
